fa0d8129ee86b2bd920b9a5bbc34a3baf0e19eb5
marniehernandez <43832443+marniehernandez@users.noreply.github.com>
2021-11-21 16:44:11 -0800

    Update prog3.cpp

--- prog3.cpp
+++ prog3.cpp
@@ -22,45 +22,170 @@ using namespace std::chrono;
 
 
 
+std::tuple<double, std::string, std::string> matching_score(double m, double c, double d, std::string a, std::string b, 
+                              double current_score, std::string a_matched, std::string b_matched, double worst_case)
+{
+    //the answer f
+    std::tuple<double, std::string, std::string> answer(current_score, a_matched, b_matched);
+
+    cout << std::get<1>(answer) << "\n";
+    cout << std::get<2>(answer) << "\n";
+    cout << "current score: " <<std::get<0>(answer) << "\n";
+    cout << "worst-case:    " << worst_case << "\n";
+    cout << "\n";
+
+
+    //end of sequence
+    if( a.length() == 0 && b.length() == 0 )
+    {
+        return answer;
+    }
+    //end of a; b is longer, and therefore rest of a was deleted
+    else if( a.length() == 0 )
+    {
+        std::get<0>(answer) += d;
+        std::get<1>(answer) += "_";
+        std::get<2>(answer) += b[0];
+
+        return matching_score(m,c,d,a,b.substr(1),
+                                std::get<0>(answer),std::get<1>(answer),std::get<2>(answer), worst_case);
+    }
+    //end of b; a is longer, and therefore rest of b was deleted
+    else if( b.length() == 0 )
+    {
+        std::get<0>(answer) += d;
+        std::get<1>(answer) += a[0];
+        std::get<2>(answer) += "_";
+
+        return matching_score(m,c,d,a.substr(1),b,
+                                std::get<0>(answer),std::get<1>(answer),std::get<2>(answer), worst_case);
+    }
+    //characters are matched, and this branch does not split
+    else if( a[0] == b[0] )
+    {
+        std::get<0>(answer) += m;
+        std::get<1>(answer) += a[0];
+        std::get<2>(answer) += b[0];
+        
+        //re-calculate w using the remaning sub-strings
+        double min_deletions = d * std::abs( (int)a.length()-1 - (int)b.length()-1 );
+        double max_changes = c * std::min( (int)a.length()-1, (int)b.length()-1 );
+        double w = max_changes + min_deletions;
+        if(current_score > 0)
+            w += current_score;
+
+        return matching_score(m,c,d,a.substr(1),b.substr(1),
+                                std::get<0>(answer),std::get<1>(answer),std::get<2>(answer), w);
+    }
+    //lower than the worst-case score, return here and reduce current_score by 1
+    //to prevent this case from being chose at the end
+    if( current_score < worst_case )
+    {
+        std::get<0>(answer) -= 1.0;
+        return answer;
+    }
+    //characters are different, and this is the end of this sub-problem
+    else if( (a[0] != b[0]) && (a.length() == 1) && (b.length() == 1) )
+    {
+        std::get<0>(answer) += c;
+        std::get<1>(answer) += a[0];
+        std::get<2>(answer) += "R";
+
+        return answer;
+    }
+    //characters are not matched inside the problem, and we must choose between a deletion in a
+    //a deletion in b, and a mismatch
+    else
+    {
+        double mismatch_score = current_score + c;
+        std::string a_mismatched = a_matched + a[0];
+        std::string b_mismatched = b_matched + "R";
+        double deletion_score = current_score + d;
+        std::string a_deleted = a_matched + "_";
+        std::string b_deleted = b_matched + "_";
+        std::string a_not_deleted = a_matched + a[0];
+        std::string b_not_deleted = b_matched + b[0];
+
+        auto mismatch = matching_score(m,c,d,a.substr(1),b.substr(1),
+                                mismatch_score,a_mismatched,b_mismatched, worst_case);
+        auto delete_a = matching_score(m,c,d,a,b.substr(1),
+                                deletion_score,a_deleted,b_not_deleted, worst_case);
+        auto delete_b = matching_score(m,c,d,a.substr(1),b,
+                                deletion_score,a_not_deleted,b_deleted, worst_case);
+
+        double mismatch_branch_score = std::get<0>(mismatch);
+        double delete_a_score = std::get<0>(delete_a);
+        double delete_b_score = std::get<0>(delete_b);
+
+            
+        if( (mismatch_branch_score >= delete_a_score) && (mismatch_branch_score >= delete_b_score) )
+        {
+            return mismatch;
+        }
+        else
+        if( (delete_a_score >= mismatch_branch_score) && (delete_a_score >= delete_b_score) )
+        {
+            return delete_a;
+        }
+        else
+        {
+            return delete_b;
+        }
+    }
+}
+
+
+
 int main(int argc, char *argv[])
 {    
     //obtain the coordinates of all points as a pair
+    auto start = high_resolution_clock::now();
+
     double score = 0;
     std::string a_compared = "";
     std::string b_compared = "";
 
+    /*
     double m = std::stod(argv[1]);
     double c = std::stod(argv[2]);
     double d = std::stod(argv[3]);
     std::string a = argv[4];
     std::string b = argv[5];
+    */
 
-    std::cout << "m: " << m << "\n";
-    std::cout << "c: " << c << "\n";
-    std::cout << "d: " << d << "\n";
-    std::cout << "a: " << a << "\n";
-    std::cout << "b: " << b << "\n";
+    //DEBUG VERSION OF DECLARATIONS FOR RUNNING OFF CSIL
+    double m = 2;
+    double c = -0.5;
+    double d = -1;
+    std::string a = "ACACACTAC";
+    std::string b = "AGCACACAG";
+    /*
+    A=
+        8.5
+        A_CACACTA
+        AGCACAC_A
+    */
+    //to prevent very long branches
+    //it is best to assume a change across the two shared slots
+    //and then perform deletions with the bases left over
+    //then to perform too many deletions
+    //our worst-case score w is equal to the maximum number of changes,
+    //and the minimum number of deletions
+    double min_deletions = d * std::abs( (int)a.length() - (int)b.length() );
+    double max_changes = c * std::min( (int)a.length(), (int)b.length() );
+    double w = max_changes + min_deletions;
+    
+    auto answer = matching_score(m, c, d, a, b, 0, "", "", w);
 
-    int l_a = a.length();
-    int l_b = b.length();
-    int l_diff = l_a - l_b;
+    cout << std::get<0>(answer) << "\n";
+    cout << std::get<1>(answer) << "\n";
+    cout << std::get<2>(answer) << "\n";
 
-    int i1_a = 0;
-    int i1_b = 0;
-    int i2_a = 0;
-    int i2_b = 0;
+    auto end = high_resolution_clock::now();
+    auto total_runtime = duration_cast<microseconds>(end - start);
+    double runtime_in_seconds = (double)total_runtime.count() / 1000000.0;
 
-    while((i1_a < l_a) &&(i1_b < l_b))
-    {
-        //characters are matched
-        if( a[i1_a] == b[i1_a] )
-        {
-            score += m;
-        }
-        i1_a++;
-        i2_a++;
-    }
-    std::cout << score << "\n";
-
+    cout << "Runtime: " << runtime_in_seconds << " s\n";
+    
     return 0;
 }
